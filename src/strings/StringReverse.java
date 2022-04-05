285d23f13a10a6b1b2d2bb4b9070748b230539d7
Djeymisson <djeymisson@gmail.com>
2022-04-05 15:59:01 -0300

    refactor(strings): removing redundant method

--- src/strings/StringReverse.java
+++ src/strings/StringReverse.java
@@ -1,55 +1,37 @@
 package strings;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class StringReverse {
 
-    public static String checkIfPalindrome(String A){
-        for (int i = 0; i < A.length() / 2; i++) {
-            if(A.charAt(i) != A.charAt(A.length()-1-i)){
+    public static String checkIfPalindrome(String a) {
+        for (int i = 0; i < a.length() / 2; i++) {
+            if (a.charAt(i) != a.charAt(a.length() - 1 - i)) {
                 return "No";
             }
         }
         return "Yes";
     }
 
-    public static String checkIfPalindrome2(String A){
-        String result = "Yes";
-        for (int i = 0; i < A.length() / 2; i++) { // needs to iterate only till half string
-            if(A.charAt(i) != A.charAt(A.length()-1-i)){
-                result = "No";
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static String checkIfPalindrome3(String A){
-        return A.equals(new StringBuilder(A).reverse().toString()) ? "Yes" : "No";
+    public static String checkIfPalindrome2(String a) {
+        return a.equals(new StringBuilder(a).reverse().toString()) ? "Yes" : "No";
     }
 
     // Checks if is palindrome
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
 
         /* Enter your code here. Print output to STDOUT. */
         long start = System.nanoTime();
-        System.out.println(checkIfPalindrome(A));
+        System.out.println(checkIfPalindrome(a));
         long end = System.nanoTime();
-        System.out.printf("Time of execution: %d%n", (end-start));
+        System.out.printf("Time of execution: %d%n", (end - start));
 
         start = System.nanoTime();
-        System.out.println(checkIfPalindrome2(A));
+        System.out.println(checkIfPalindrome2(a));
         end = System.nanoTime();
-        System.out.printf("Time of execution: %d%n", (end-start));
-
-        start = System.nanoTime();
-        System.out.println(checkIfPalindrome3(A));
-        end = System.nanoTime();
-        System.out.printf("Time of execution: %d%n", (end-start));
-
+        System.out.printf("Time of execution: %d%n", (end - start));
 
     }
 }
