882a076b30d9a0fa230c0a813e232d0a6f011eb9
Djeymisson <djeymisson@gmail.com>
2022-04-12 22:42:54 -0300

    feat(advanced): adding varargs example

---
+++ src/advanced/JavaVarargs.java
@@ -0,0 +1,59 @@
+package advanced;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Add {
+    void add(int... n) {
+        int sum = 0;
+        StringBuilder output = new StringBuilder();
+
+        for (int i : n) {
+            output.append(i).append("+");
+            sum += i;
+        }
+
+        output = new StringBuilder(output.toString().replaceAll("\\+$", "=" + sum));
+        System.out.println(output);
+    }
+}
+
+public class JavaVarargs {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+
+            }
+            if (overload) throw new RuntimeException("Overloading not allowed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
